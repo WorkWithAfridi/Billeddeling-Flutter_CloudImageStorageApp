6e6c6067ac2201ffdf35d8415e969d8ff8a7c0cf
Kyoto <khondakarafridi35@gmail.com>
2022-08-05 09:35:24 +0600

    Code refactoring

---
+++ lib/app/shared/widgets/app_icon_and_title.dart
@@ -0,0 +1,53 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
+import 'package:flutter/material.dart';
+
+import '../../data/constants/colors.dart';
+import '../../data/constants/fonts.dart';
+
+class AppIconAndTitle extends StatelessWidget {
+  const AppIconAndTitle({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Icon(
+          FluentIcons.camera_16_regular,
+          size: 40,
+          color: red,
+        ),
+        const SizedBox(
+          width: 2,
+        ),
+        Container(
+          height: 35,
+          width: 0.5,
+          color: navyBlue.withOpacity(.5),
+        ),
+        const SizedBox(
+          width: 5,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Billeddeling",
+              style: semiBoldTextStyle,
+            ),
+            Text(
+              "Image Sharing App",
+              style: regularTextStyle.copyWith(
+                height: .7,
+                fontSize: 12,
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
--- lib/app/views/splash_screen/splash_screen.dart
+++ lib/app/views/splash_screen/splash_screen.dart
@@ -1,8 +1,7 @@
-import 'package:billeddeling/app/data/constants/colors.dart';
-import 'package:billeddeling/app/data/constants/fonts.dart';
-import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../../shared/widgets/app_icon_and_title.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -13,44 +12,7 @@ class SplashScreen extends StatelessWidget {
       body: SizedBox(
         height: Get.height,
         width: Get.width,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(
-              FluentIcons.camera_16_regular,
-              size: 40,
-              color: red,
-            ),
-            const SizedBox(
-              width: 2,
-            ),
-            Container(
-              height: 35,
-              width: 0.5,
-              color: navyBlue.withOpacity(.5),
-            ),
-            const SizedBox(
-              width: 5,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Billeddeling",
-                  style: semiBoldTextStyle,
-                ),
-                Text(
-                  "Image Sharing App",
-                  style: regularTextStyle.copyWith(
-                    height: .7,
-                    fontSize: 12,
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
+        child: const AppIconAndTitle(),
       ),
     );
   }
