f4d8e4e1f6a5ead9d5c597aea6f19a6b03b7596c
khondakar.afridi <afridi.khondakar@gmail.com>
2024-02-10 20:17:18 +0600

    Firebase integrated

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:billeddeling/app/data/constants/animations.dart';
 import 'package:billeddeling/app/data/theme/theme.dart';
 import 'package:billeddeling/app/routing/routes.dart';
+import 'package:billeddeling/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const BilleddelingApp());
 }
 
 class BilleddelingApp extends StatelessWidget {
-  const BilleddelingApp({Key? key}) : super(key: key);
+  const BilleddelingApp({super.key});
 
   @override
   Widget build(BuildContext context) {
